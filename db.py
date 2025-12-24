2173f33aae27adcd58e2c9bf152f0a5b83a70a72
jam06452 <danjward8@gmail.com>
2025-12-24 23:41:13 +0000

    Added better docker practices, cache pip etc

--- db.py
+++ db.py
@@ -1,22 +1,25 @@
 from supabase import create_client, Client
 
-url = "http://localhost:8000"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q"
+#For Testing locally:
+#Will be changed to my prod server
+URL = "http://localhost:8000"
 
-SUPABASE: Client = create_client(url, key)
+#Not ready for prod yet. Key is there for ease, will be using a .env file if deployed.
+#DB is hosted locally, no external connections atm
+KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q"
+
+#Using Constant for SUPABASE creds since it will never be changed by the script. Used multiple times
+SUPABASE: Client = create_client(URL, KEY)
 
 def save(encoded, decoded):
-    global SUPABASE
-
+    #Date is handled by supabase incase the script is ran in a different timezone by accident
     data = {"Encoded": encoded, "Decoded": decoded}
     response = SUPABASE.table("URL_Shortener").insert(data).execute()
 
     return response
 
-
+#Returns an encoded string from an entire url
 def read_encode(decoded):
-    global SUPABASE
-
     response = SUPABASE.table("URL_Shortener").select("Encoded").eq("Decoded", decoded).execute()
     
     if response.data:
@@ -24,9 +27,8 @@ def read_encode(decoded):
     else:
         return None
 
+#Returns an decoded url from an entire encoded string
 def read_decode(encoded):
-    global SUPABASE
-
     response = SUPABASE.table("URL_Shortener").select("Decoded").eq("Encoded", encoded).execute()
 
     if response.data:
---
+++ test.py
@@ -0,0 +1,3 @@
+import db
+
+print(db.read_encode("https://google.com"))
